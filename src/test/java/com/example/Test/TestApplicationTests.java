b946d7bc5782fc593fb3d28cf9edbb2efbf0ca4a
gaurav <egaurav.mails@gmail.com>
2021-06-05 20:04:34 -0400

    second commit

--- src/test/java/com/example/Test/TestApplicationTests.java
+++ src/test/java/com/example/Test/TestApplicationTests.java
@@ -1,13 +1,18 @@
 package com.example.Test;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TestApplicationTests {
+	private final Logger logger= LoggerFactory.getLogger(this.getClass());
 
 	@Test
 	void contextLoads() {
+		logger.info("In Test Class.....");
+
 	}
 
 }
